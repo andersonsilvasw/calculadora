10f13ba794df663121b5b4996e9d8f0caca2a6a0
andersonsilvasw <andersonsilvaedif@hotmail.com>
2022-12-20 13:34:44 -0300

    Lógica da caulculadora #02 - Aula 22

--- src/br/com/cod3r/calc/modelo/Memoria.java
+++ src/br/com/cod3r/calc/modelo/Memoria.java
@@ -1,7 +1,12 @@
 package br.com.cod3r.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 	private static final Memoria instancia = new Memoria();
+	
+	private final List<MemoriaObservador> observadores = new ArrayList<>();
 	
 	private String textoAtual = "";
 	
@@ -12,8 +17,22 @@ public class Memoria {
 	public static Memoria getInstancia() {
 		return instancia;
 	}
+	
+	public void adicionarObservador(MemoriaObservador observador) {
+		observadores.add(observador);
+	}
 
 	public String getTextoAtual() {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}	
+	
+	public void processarComando(String valor) {
+		
+		if("AC".equals(valor)) {
+			textoAtual = "";
+		} else {
+			textoAtual += valor;
+		}	
+		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
 }
